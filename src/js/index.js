21d67ad4207204b2c9bba0d332cd6bbec74cf70a
John Chacpi <johnchacpi479@gmail.com>
2023-01-28 23:29:58 -0500

    created grid, added dynamic board size

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,21 @@
-const gridContainer = document.querySelector(".grid-container");
+function populate(size) {
+    let gridContainer = document.querySelector(".grid-container");
+    const prevCells = document.querySelectorAll(".cell");
+    prevCells.forEach((cell) => cell.remove())
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    for (let i = 0; i<Math.pow(size, 2); i++) {
+        let cell = document.createElement("div");
+        cell.style.backgroundColor = "blue";
+        cell.classList.add("cell");
+        cell.style.border = "1px solid black"
+        gridContainer.insertAdjacentElement("beforeend", cell)
+    }
+}
+
+populate(16);
+
+function changeSize(input) {
+    populate(input)
+}
+
