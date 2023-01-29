3ced200e70e998f444c4adae77ace638e1f9056c
John Chacpi <johnchacpi479@gmail.com>
2023-01-29 12:12:55 -0500

    change color functionality and rainbow feature added

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+let currentColor = "black";
+
 function populate(size) {
     let gridContainer = document.querySelector(".grid-container");
     const prevCells = document.querySelectorAll(".cell");
@@ -6,8 +8,9 @@ function populate(size) {
     gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     for (let i = 0; i<Math.pow(size, 2); i++) {
         let cell = document.createElement("div");
-        cell.style.backgroundColor = "blue";
+        cell.style.backgroundColor = "whte";
         cell.classList.add("cell");
+        cell.addEventListener("mouseover", colorCell)
         cell.style.border = "1px solid black"
         gridContainer.insertAdjacentElement("beforeend", cell)
     }
@@ -19,8 +22,22 @@ function changeSize(input) {
     if(input >= 2 && input <= 100) {
         populate(input)
     }
+    else if (input < 0) {
+        console.log("We haven't invented negative areas yet")
+    }
     else {
         console.log("Maximun number of cells reached")
     }
 }
+function colorCell() {
+    if (color = "rainbow") {
+        this.style.backgroundColor = `hsl(${Math.random()*360}, 100%, 50%)`
+    }
+    else {
+        this.style.backgroundColor = currentColor;
+    }
+}
 
+function changeColor(color) {
+    currentColor = color
+}
